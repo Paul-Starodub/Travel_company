2e9c88ec1da68d70bc2f7c3be560d60f4f3d4cc5
Pavlo Starobub <amorallex@gmail.com>
2026-06-05 18:34:10 +0300

    refactor prod.py

--- travel_company/settings/prod.py
+++ travel_company/settings/prod.py
@@ -19,13 +19,3 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-
-# Static (Whitenoise)
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
