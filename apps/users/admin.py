32c01238db38ec804ec63538964d2ae8b7934fb2
Pavlo Starobub <amorallex@gmail.com>
2026-06-05 17:09:56 +0300

    add users app with user model

---
+++ apps/users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "is_staff", "is_active", "date_joined"]
+    list_filter = ["is_staff", "is_superuser", "is_active"]
+    search_fields = ["username", "email", "first_name", "last_name"]
+    ordering = ["-date_joined"]
+    readonly_fields = ["date_joined", "last_login"]
